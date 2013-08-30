7ae2d8ea4442a62ed110326d48054c4cdfa77b4f
Christopher Grebs <cg@webshox.org>
2013-08-30 10:34:11 -0700

    Remove anaconda, not working :-/

--- sublime-text-3/Packages/Anaconda/anaconda_client/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf8 -*-
-
-# Copyright (C) 2013 - Oscar Campos <oscar.campos@member.fsf.org>
-# This program is Free Software see LICENSE file for details
-
-from .jsonclient import AsynClient
-
-
-__all__ = ['AsynClient']
